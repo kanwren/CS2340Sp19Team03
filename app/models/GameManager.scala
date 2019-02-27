bf0a2c69dfa8658f984d82cbcfeb4166cd2591b5
Ishan Arya <aryaishn@gmail.com>
2019-02-27 11:15:52 -0500

    Fixed tabbing

--- app/models/GameManager.scala
+++ app/models/GameManager.scala
@@ -4,23 +4,23 @@ import scala.collection.mutable
 import scala.util.Random
 
 object GameManager {
-  val games: mutable.HashMap[String, Game] = mutable.HashMap[String, Game]()
+    val games: mutable.HashMap[String, Game] = mutable.HashMap[String, Game]()
 
-  def getGameById(gameId: String): Option[Game] = games get gameId
+    def getGameById(gameId: String): Option[Game] = games get gameId
 
-  def makeNewGame: String = {
-    val id = generateId
-    games += id -> new Game(id)
-    id
-  }
-
-  private def generateId: String = {
-    val id = Random.alphanumeric.take(Game.idLength).mkString("")
-    if (games contains id) {
-      generateId
+    def makeNewGame: String = {
+        val id = generateId
+        games += id -> new Game(id)
+        id
     }
-    else {
-      id
+
+    private def generateId: String = {
+        val id = Random.alphanumeric.take(Game.idLength).mkString("")
+        if (games contains id) {
+            generateId
+        }
+        else {
+            id
+        }
     }
-  }
 }

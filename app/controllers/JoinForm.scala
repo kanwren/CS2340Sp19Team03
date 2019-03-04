f43018c56d9b06638d573d8e5caafc092f508c9e
Justin Prindle <jprindle6@gatech.edu>
2019-03-03 19:52:54 -0500

    Fix inconsistent indentation

--- app/controllers/JoinForm.scala
+++ app/controllers/JoinForm.scala
@@ -1,20 +1,16 @@
 package controllers
 
 object JoinForm {
-    import play.api.data.Forms._
-    import play.api.data.Form
 
-    case class GameId(id: String)
+  import play.api.data.Form
+  import play.api.data.Forms._
 
-    /**
-      * The form definition for the "create a widget" form.
-      * It specifies the form fields and their types,
-      * as well as how to convert from a Data to form data and vice versa.
-      */
-    val form: Form[GameId] = Form(
-        mapping(
-            "gameId" -> nonEmptyText
-        )(GameId.apply)(GameId.unapply)
-    )
+  case class GameId(id: String)
+
+  val form: Form[GameId] = Form(
+    mapping(
+      "gameId" -> nonEmptyText
+    )(GameId.apply)(GameId.unapply)
+  )
 }
 
--- app/models/GameManager.scala
+++ app/models/GameManager.scala
@@ -4,23 +4,22 @@ import scala.collection.mutable
 import scala.util.Random
 
 object GameManager {
-    val games: mutable.HashMap[String, Game] = mutable.HashMap[String, Game]()
+  val games: mutable.HashMap[String, Game] = mutable.HashMap[String, Game]()
 
-    def getGameById(gameId: String): Option[Game] = games get gameId
+  def getGameById(gameId: String): Option[Game] = games get gameId
 
-    def makeNewGame: String = {
-        val id = generateId
-        games += id -> new Game(id)
-        id
+  def makeNewGame: String = {
+    val id = generateId
+    games += id -> new Game(id)
+    id
+  }
+
+  private def generateId: String = {
+    val id = Random.alphanumeric.take(Game.idLength).mkString("")
+    if (games contains id) {
+      generateId
+    } else {
+      id
     }
-
-    private def generateId: String = {
-        val id = Random.alphanumeric.take(Game.idLength).mkString("")
-        if (games contains id) {
-            generateId
-        }
-        else {
-            id
-        }
-    }
+  }
 }

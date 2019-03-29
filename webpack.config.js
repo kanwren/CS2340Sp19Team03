41d6a1972609fa94efe7cdb0efc52ae8b5ca5e79
Jeffrey Luo <luojeffrey@yahoo.com>
2019-03-29 00:50:01 -0400

    Start end turn functionality in sending AJAX request

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: ['babel-polyfill', './ui/entry.js'],
-    output: { path: path.resolve(__dirname, 'public/compiled'), filename: 'bundle.js' },
+    entry: './ui/entry.js',
+    output: {path: path.resolve(__dirname, 'public/compiled'), filename: 'bundle.js'},
     module: {
-        rules: [ {
-                test: /\.jsx?$/,
-                include: /ui/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [ 'react', 'es2015', 'stage-0' ]
-                    }
+        rules: [{
+            test: /\.jsx?$/,
+            include: /ui/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015', 'stage-0']
                 }
-        } ]
+            }
+        }]
     }
 };

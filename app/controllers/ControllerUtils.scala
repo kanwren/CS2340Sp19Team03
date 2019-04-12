0594fee854ef727cb3e7f22ab81abdc6d24767bf
Justin Prindle <jprindle6@gatech.edu>
2019-04-12 12:02:59 -0400

    Add ControllerUtils documentation

--- app/controllers/ControllerUtils.scala
+++ app/controllers/ControllerUtils.scala
@@ -6,9 +6,24 @@ import play.api.mvc._
 
 trait ControllerUtils {
 
+  /**
+    * Loan pattern to lookup a game by ID and perform some continuation on the
+    * result, if it exists, redirecting to an error page if it does not.
+    *
+    * @param gameId the ID of the game to look up
+    * @param handler continuation to get a result from a game, if it exists
+    * @return the result of {@code handler}, if the game exists, or an error
+    *      redirection otherwise
+    */
   def onGame(gameId: String)(handler: Game => Result): Result =
     GameManager.getGameById(gameId).fold(redirectInvalidGameId(gameId))(handler)
 
+  /**
+    * Handles invalid game ID lookups by redirecting to the index with an error
+    * message
+    * @param gameId the game ID that did not exist
+    * @return an error redirection
+    */
   def redirectInvalidGameId(gameId: String): Result =
     Redirect(routes.GameController.index()).flashing("ERROR" -> s"Could not find game with game ID '$gameId'")
 

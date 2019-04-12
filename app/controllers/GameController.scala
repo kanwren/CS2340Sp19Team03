17951d546f431194262c6028d21f670767841d8a
Justin Prindle <jprindle6@gatech.edu>
2019-04-11 23:57:50 -0400

    Make testGame route

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -7,20 +7,27 @@ import akka.stream.Materializer
 import javax.inject.Inject
 import models._
 import play.api.data._
-import play.api.libs.functional.syntax._
-import play.api.libs.json._
 import play.api.mvc._
 
 class GameController @Inject()(cc: MessagesControllerComponents)
                               (implicit system: ActorSystem, mat: Materializer) extends MessagesAbstractController(cc) with ControllerUtils {
+
+  def testGame(players: Int): Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
+    val game = GameManager.makeNewGame
+    val playerNames: Seq[String] = ('A' to 'Z') map { _.toString } take players
+    playerNames foreach { game.addPlayerToLobby }
+    game.startAssignment()
+    game.startPlay()
+    Redirect(routes.GameController.showGame(game.gameId, Some("A")))
+  }
 
   def index: Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok(views.html.index(JoinForm.form))
   }
 
   def createGame: Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
-    val id = GameManager.makeNewGame
-    Redirect(routes.GameController.index()).flashing("INFO" -> s"Your game ID is: $id")
+    val game = GameManager.makeNewGame
+    Redirect(routes.GameController.index()).flashing("INFO" -> s"Your game ID is: ${game.gameId}")
   }
 
   def joinGame: Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
@@ -81,6 +88,7 @@ class GameController @Inject()(cc: MessagesControllerComponents)
       Redirect(routes.GameController.showGame(gameId))
     }
   }
+
   def addArmiesToTerritory(gameId: String, territoryId: Int, amount: Int): Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
     onGame(gameId) { game: Game =>
       game.board.territories(territoryId).armies += amount
